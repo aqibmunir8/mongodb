67b290b4badd7db264f4df865f470d555c253bdd
Aqib Munir <aqibmunir724@gmail.com>
2025-08-31 14:36:47 +0500

    feat: add examples for 1-1 and 1-M relationships in MongoDB

---
+++ dbrelationship/models/customer_onetoM.js
@@ -0,0 +1,63 @@
+// One to Many
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+main()
+  .then(() => console.log("Connection Successful"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
+}
+
+const orderSchema = new Schema({
+    item: String,
+    price: Number,
+});
+
+const customerSchema = new Schema({
+    name: String, 
+    orders: [{ type: Schema.Types.ObjectId, ref: 'Order', }]
+})
+
+
+const order = mongoose.model("Order", orderSchema);
+const customer = mongoose.model("Customer", customerSchema);
+
+// const addOrders = async () =>{
+//     let res = await order.insertMany([
+//         { item: "Samosa", price: 12},
+//         { item: "Juice", price: 29},
+//         { item: "Chocolate", price: 40},
+//     ])
+//     console.log(res);
+// };
+
+// addOrders();
+
+
+const addCustomer = async() => {
+    let cust1 = new customer({
+        name: "Rami Malik"
+    }) 
+
+    let order1 = await order.findOne({item: "Samosa"});
+    let order2 = await order.findOne({item: "Juice"});
+
+    cust1.orders.push(order1);
+    cust1.orders.push(order2);
+
+    console.log(cust1);
+
+    let result = await cust1.save();
+    console.log(cust1);
+}
+
+addCustomer();
+
+let custFind = async () => {
+    let result = await customer.find({});
+    console.log(result)
+};
+
+custFind();
